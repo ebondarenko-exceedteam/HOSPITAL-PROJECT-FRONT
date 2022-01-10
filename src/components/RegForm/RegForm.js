7c09bdfe3ca717568354131f2cc8a71e5bed7e4b
ebondarenko-exceedteam <ebondarenko.exceedteam@gmail.com>
2022-01-10 20:03:57 +0300

    add react-hook-form on RegForm

--- src/components/RegForm/RegForm.js
+++ src/components/RegForm/RegForm.js
@@ -1,19 +1,83 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import './RegForm.scss'
 
 const RegForm = () => {
-  const handleSubmit = () => {}
+  const {
+    register,
+    formState: {
+      errors
+    },
+    reset,
+    handleSubmit
+  } = useForm({
+    mode: 'onBlur'
+  });
+
+  const hendlerSubmit = (data) => {
+    alert(JSON.stringify(data));
+    reset();
+  }
 
   return (
     <div className='regForm'>
       <p className='regForm_title'>Регистрация</p>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(hendlerSubmit)}>
         <label>Login:</label>
-        <input type='text' id='regFormLogin' name='regFormLogin' placeholder='Login' />
+        <input
+          id='regFormLogin'
+          placeholder='Login'
+          {...register('regFormLogin', {
+            required: 'Поле обязательно к заполнению',
+            minLength: {
+              value: 6,
+              message: 'Минимум 6 символов'
+            }
+          })}
+        />
+        <div className='regForm_error'>
+          {errors.regFormLogin && <p>{errors.regFormLogin.message || 'Error!'}</p>}
+        </div>
         <label>Password:</label>
-        <input type='password' id='regFormPassword' name='regFormPassword' placeholder='Password' />
+        <input
+          type='password'
+          id='regFormPassword'
+          placeholder='Password'
+          {...register('regFormPassword', {
+            required: 'Поле обязательно к заполнению',
+            minLength: {
+              value: 6,
+              message: 'Минимум 6 символов'
+            },
+            pattern: {
+              value: /^(?=.*\d)[a-zA-Z\d]{6,25}$/,
+              message: 'Только латинские буквы и минимум 1 цифра'
+            }
+          })}
+        />
+        <div className='regForm_error'>
+          {errors.regFormPassword && <p>{errors.regFormPassword.message || 'Error!'}</p>}
+        </div>
         <label>Repeat password:</label>
-        <input type='password' id='regFormRepeatPassword' name='regFormRepeatPassword' placeholder='Password' />
+        <input
+          type='password'
+          id='regFormRepeatPassword'
+          placeholder='Password'
+          {...register('regFormRepeatPassword', {
+            required: 'Поле обязательно к заполнению',
+            minLength: {
+              value: 6,
+              message: 'Минимум 6 символов'
+            },
+            pattern: {
+              value: /^(?=.*\d)[a-zA-Z\d]{6,25}$/,
+              message: 'Только латинские буквы и минимум 1 цифра'
+            }
+          })}
+        />
+        <div className='regForm_error'>
+          {errors.regFormRepeatPassword && <p>{errors.regFormRepeatPassword.message || 'Error!'}</p>}
+        </div>
         <button>Зарегистрироваться</button>
         <p className='regForm_link'>Авторизоваться</p>
       </form>
